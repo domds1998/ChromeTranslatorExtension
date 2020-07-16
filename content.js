bc324d64b29a3d4276aee07ada29dcd47814f8d2
Domds1998 <dominik.suchner@gmail.com>
2020-07-16 17:36:49 +0200

    Javascript chrome extension - init

---
+++ content.js
@@ -0,0 +1,24 @@
+window.addEventListener('mouseup', mouseUpped);
+const body = document.body;
+const popupIconNode = document.createElement('div');
+popupIconNode.classList.add('iconPopup');
+
+popupIconNode.onclick = clickedOnPopupIcon;
+
+function mouseUpped() {
+    let selectedText = window.getSelection().toString();
+    let mousePositionX = event.pageX;
+    let mousePositionY = event.pageY;
+    if (selectedText.length > 0 || selectedText != "") {
+        body.appendChild(popupIconNode);
+        popupIconNode.style.top = (mousePositionY + 10) + 'px';
+        popupIconNode.style.left = mousePositionX + 'px';
+    } else {
+        if (popupIconNode.parentNode != null)
+            popupIconNode.parentNode.removeChild(popupIconNode);
+    }
+}
+
+function clickedOnPopupIcon(){
+    console.log("No i fajnie");
+}
